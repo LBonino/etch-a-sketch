25d2a4d35cd191fbc7297fbbc81db3041342636e
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-07-08 20:03:52 -0300

    Add function to insert elements into the grid

--- src/static/js/etch-a-sketch.js
+++ src/static/js/etch-a-sketch.js
@@ -1,1 +1,17 @@
-let squareGridSize = 16;
+let squareGridSize = 16**2;
+const grid = document.querySelector("#grid-container");
+
+function insertSquareGridElements(grid, squareGridSize) {
+    grid.style.gridTemplate = `repeat(${squareGridSize**(1/2)}, 1fr) /
+                               repeat(${squareGridSize**(1/2)}, 1fr)`;
+
+    for (let i = 0; i < squareGridSize; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.classList.add("grid-element");
+        gridElement.style.borderBottom = "solid 0.1px rgba(0, 0, 0, 0.9)";
+        gridElement.style.borderRight = "solid 0.1px rgba(0, 0, 0, 0.9)";
+        grid.appendChild(gridElement);
+    }
+}
+
+insertSquareGridElements(grid, squareGridSize);
