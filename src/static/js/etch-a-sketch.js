a392181cd1211e36cc4f353b12f0df5b9e40405e
Lisandro Bonino <lisandrobonino4@protonmail.com>
2021-07-10 11:45:17 -0300

    Add data attribute 'color'  to each grid element This data attribute will be used later to determine which color should be used to paint the grid. Default value is black but it could be changed clicking the 'random' or 'pick' button.

--- src/static/js/etch-a-sketch.js
+++ src/static/js/etch-a-sketch.js
@@ -5,6 +5,7 @@ function insertSquareGridElements(grid, squareGridSize) {
     for (let i = 0; i < squareGridSize; i++) {
         const gridElement = document.createElement("div");
         gridElement.classList.add("grid-element");
+        gridElement.dataset.color = "black";
         gridElement.style.borderBottom = "solid 0.1px rgba(0, 0, 0, 0.9)";
         gridElement.style.borderRight = "solid 0.1px rgba(0, 0, 0, 0.9)";
         grid.appendChild(gridElement);
